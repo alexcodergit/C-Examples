1017da5ffa176dd62ada0025ce074e3e57f1009c
DESKTOP-08PSKHT\Besitzer <tornadoalex@hotmail.com>
2022-01-02 12:01:47 +0100

    Example for Equal added

---
+++ EqualsGetHashCode/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualsGetHashCode
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string CityOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int PassportNumber { get; set; }
+
+        public Person(string fn, string sn, string cob, DateTime dob)
+        {
+            FirstName = fn;
+            SecondName = sn;
+            CityOfBirth = cob;
+            DateOfBirth = dob;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+
+            return (FirstName == ((Person)obj).FirstName) &&
+                (SecondName == ((Person)obj).SecondName) &&
+                (DateOfBirth == ((Person)obj).DateOfBirth) &&
+                (CityOfBirth == ((Person)obj).CityOfBirth);
+        }
+
+        public override int GetHashCode()
+        {
+            return FirstName.GetHashCode() ^
+                SecondName.GetHashCode() ^
+                CityOfBirth.GetHashCode() ^
+                DateOfBirth.GetHashCode();
+        }
+
+        public static bool operator !=(Person p1, Person p2)
+        {
+            return !(p1 == p2);
+        }
+        public static bool operator ==(Person p1, Person p2)
+        {
+            if (ReferenceEquals(p1, p2))
+            {
+                return true;
+            }
+            if (p1 is null || p2 is null)
+            {
+                return false;
+            }
+            return p1.Equals(p2);
+        }
+    }
+}
---
+++ EqualsGetHashCode/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EqualsGetHashCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new("P1", "P2", "city1", new DateTime(1980, 10, 10));
+            person1.PassportNumber = 1;
+            Person person2 = new("P1", "P2", "city1", new DateTime(1980, 10, 10));
+            person2.PassportNumber = 2;
+            if (person1.Equals(person2))
+            {
+                Console.WriteLine("Person1.Equals(Person2), even when passport numbers are not equal.");
+            }
+            if (person1 == person2)
+            {
+                Console.WriteLine("Person1 == Person2, because operator== is overloaded.");
+            }
+            CheckStringEquality();
+            Console.ReadKey();
+        }
+
+        static void CheckStringEquality()
+        {
+            string a = new("abc");
+            string b = new("abc");
+
+            if (a == b)
+            {
+                Console.WriteLine("Method Equal for strings is properly defined");
+            }
+            else
+            {
+                Console.WriteLine("Method Equal for strings is not properly defned");
+            }
+        }
+    }
+}
