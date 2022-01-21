02d0b58abbf0b0803ba24bd9144756880e43fc28
Andrushenko Alexander (ETAS-DAP/XPC-Fe1) <Alexander.Andrushenko@etas.com>
2022-01-21 14:44:33 +0100

    Casting changed

--- Casting/Program.cs
+++ Casting/Program.cs
@@ -4,7 +4,7 @@ namespace Casting
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             USDollar dollar = new();
             Euro euro = new();
@@ -16,60 +16,25 @@ namespace Casting
             hrivna.UnitsPerUsd = 27.234;
 
             int amount = 10;
-            double converted = ConvertCurrency(euro, dollar, amount);
+            double converted = Currency.ConvertCurrency(euro, dollar, amount);
             Console.WriteLine($"{amount} {euro.GetCurrencyName()} = {converted} {dollar.GetCurrencyName()}");
 
-            converted = ConvertCurrency(dollar, euro, amount);
+            converted = Currency.ConvertCurrency(dollar, euro, amount);
             Console.WriteLine($"{amount} {dollar.GetCurrencyName()} = {converted} {euro.GetCurrencyName()}");
 
-            converted = ConvertCurrency(euro, ruble, amount);
+            converted = Currency.ConvertCurrency(euro, ruble, amount);
             Console.WriteLine($"{amount} {euro.GetCurrencyName()} = {converted} {ruble.GetCurrencyName()}");
 
-            converted = ConvertCurrency(ruble, euro, amount);
+            converted = Currency.ConvertCurrency(ruble, euro, amount);
             Console.WriteLine($"{amount} {ruble.GetCurrencyName()} = {converted} {euro.GetCurrencyName()}");
 
-            converted = ConvertCurrency(hrivna, dollar, amount);
+            converted = Currency.ConvertCurrency(hrivna, dollar, amount);
             Console.WriteLine($"{amount} {hrivna.GetCurrencyName()} = {converted} {dollar.GetCurrencyName()}");
 
-            converted = ConvertCurrency(dollar, hrivna, amount);
+            converted = Currency.ConvertCurrency(dollar, hrivna, amount);
             Console.WriteLine($"{amount} {dollar.GetCurrencyName()} = {converted} {hrivna.GetCurrencyName()}");
 
             Console.ReadLine();
-        }
-
-        public static double ConvertCurrency(Currency from, Currency to, double amount)
-        {
-            if (from.GetType() == to.GetType())
-            {
-                return amount;
-            }
-
-            double fromInUs = amount / GetUnitsPerUSD(from);
-            double retAmount = fromInUs * GetUnitsPerUSD(to);
-            return retAmount;
-        }
-        static double GetUnitsPerUSD(Currency cur)
-        {
-            if (cur.GetType() == typeof(Euro))
-            {
-                return ((Euro)cur).UnitsPerUsd;
-            }
-            else if (cur.GetType() == typeof(Ruble))
-            {
-                return ((Ruble)cur).UnitsPerUsd;
-            }
-            else if (cur.GetType() == typeof(Hrivna))
-            {
-                return ((Hrivna)cur).UnitsPerUsd;
-            }
-            else if ((cur.GetType() == typeof(USDollar)))
-            {
-                return ((USDollar)cur).UnitsPerUsd;
-            }
-            else
-            {
-                throw new ArgumentException($"Unknown Currency {cur.GetCurrencyName()}");
-            }
         }
     }
 }
